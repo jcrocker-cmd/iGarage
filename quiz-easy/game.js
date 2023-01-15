fa601d8c679b86edcba82cfa97259d2d6cd951c6
John Christian B. Narbaja <narbajajc@gmail.com>
2023-01-15 23:45:22 +0800

    Updated quiz game

--- quiz-easy/game.js
+++ quiz-easy/game.js
@@ -63,7 +63,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('/quiz/end.html');
+        return window.location.assign('/quiz-easy/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
