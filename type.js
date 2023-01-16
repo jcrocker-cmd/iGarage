9fa212d76f6d775e314dbce3cdd9b277979ac6f9
John Christian B. Narbaja <narbajajc@gmail.com>
2023-01-16 20:22:23 +0800

    Modified Design

---
+++ type.js
@@ -0,0 +1,5 @@
+new TypeIt("#simpleUsage", {
+    strings: "This game is developed by the CAN which stands for Cutin, Abrau and Narbaja a students from BSCS 4C.",
+    speed: 50,
+    waitUntilVisible: true,
+  }).go();
