d3edd8908cfc417c1a0d3e7c11646f3df4968f54
John Christian B. Narbaja <narbajajc@gmail.com>
2023-01-19 08:33:37 +0800

    Updated Mistakes

--- type.js
+++ type.js
@@ -1,5 +1,14 @@
 new TypeIt("#simpleUsage", {
-    strings: "This game is developed by the CAN which stands for Cutin, Abrau and Narbaja a students from BSCS 4C.",
     speed: 50,
     waitUntilVisible: true,
-  }).go();
+  })
+  .type("This game is developed by the CAN which stands for Cutin, Abrau and Narbaja a students from BSCS 4C.")
+  .break({ delay: 500 })
+  .break({ delay: 500 })
+  .type("<em>TAGLINE</em>")
+  .break({ delay: 500 })
+  .type("<em>-If we CAN then you CAN.</em>")
+  .break({ delay: 500 })
+  .break({ delay: 500 })
+  .type("<strong>© 2021 iGarage. All rights reserved.</strong>")
+  .go();
